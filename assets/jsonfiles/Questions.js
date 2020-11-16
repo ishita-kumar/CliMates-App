82f84f4f96f8e1e1e2860857f3e0393edecea0b3
Ishita Kumar <ishitakumar712@gmail.com>
2020-11-16 17:46:40 -0500

    JSON File for questiosn added

---
+++ assets/jsonfiles/Questions.js
@@ -0,0 +1,125 @@
+let arr=[{
+    { 
+        "Question_No":"1",
+        "Question_Text":"Input your electricity consumption in Kwh ",
+        "Nomber_Options":Null,
+        "Options":{
+        "Option_No":"",
+        "Option_Weight":"",
+        "Option_Text":"",
+        }
+    },
+     { 
+        "Question_No":"2",
+        "Question_Text":"Which of these devices did you remember to unplug",
+        "Nomber_Options":"5",
+        "Options":[
+        {
+        "Option_No":"1",
+        "Option_Weight":"",
+        "Option_Text":"Mobile Charger",
+        },
+         {
+        "Option_No":"2",
+        "Option_Weight":"",
+        "Option_Text":"Computer Display",
+        },
+         {
+        "Option_No":"3",
+        "Option_Weight":"",
+        "Option_Text":"Desktop",
+        },
+         {
+        "Option_No":"4",
+        "Option_Weight":"",
+        "Option_Text":"Digital Cable box",
+        },
+         {
+        "Option_No":"5",
+        "Option_Weight":"",
+        "Option_Text":"Coffee maker",
+        }
+        ]
+    },
+     { 
+       "Question_No":"3",
+        "Question_Text":"Which of the following products do you currently recycle in your household",
+        "Nomber_Options":"5",
+        "Options":[
+        {
+        "Option_No":"1",
+        "Option_Weight":"",
+        "Option_Text":"Aluminum & steel cans",
+        },
+         {
+        "Option_No":"2",
+        "Option_Weight":"",
+        "Option_Text":"Plastic",
+        },
+         {
+        "Option_No":"3",
+        "Option_Weight":"",
+        "Option_Text":"Glass",
+        },
+         {
+        "Option_No":"4",
+        "Option_Weight":"",
+        "Option_Text":"Newspaper",
+        },
+         {
+        "Option_No":"5",
+        "Option_Weight":"",
+        "Option_Text":"Magazines",
+        }
+        ]
+    },
+     { 
+       
+        "Question_No":"4",
+        "Question_Text":"Enter the amount of miles you have driven  ",
+        "Nomber_Options":Null,
+        "Options":{
+        "Option_No":"",
+        "Option_Weight":"",
+        "Option_Text":"",
+        }
+    },
+    
+     { 
+        "Question_No":"5",
+        "Question_Text":"What has been your major diet style since your last check in?",
+        "Nomber_Options":"5",
+        "Options":[
+        {
+        "Option_No":"1",
+        "Option_Weight":"",
+        "Option_Text":"Heavy Meat Eater",
+        },
+         {
+        "Option_No":"2",
+        "Option_Weight":"",
+        "Option_Text":"Average Meat Eater",
+        },
+         {
+        "Option_No":"3",
+        "Option_Weight":"",
+        "Option_Text":"Rare Meat Eater",
+        },
+         {
+        "Option_No":"4",
+        "Option_Weight":"",
+        "Option_Text":"Vegetarian",
+        },
+         {
+        "Option_No":"5",
+        "Option_Weight":"",
+        "Option_Text":"Vegan",
+        }
+        ]
+    },
+
+
+}
+]
+
+export default arr;
